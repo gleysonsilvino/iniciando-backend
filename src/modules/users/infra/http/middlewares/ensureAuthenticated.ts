0c884a7d9f1629adecdfd184430e6f31aeda78ac
gleysonsilvino <gleysonsilvino@gmail.com>
2020-11-25 10:37:18 -0300

    Pastas organizadas e add tsconfig-paths

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import AppError from '../errors/AppError';
+import authconfig from '@config/auth';
 
-import authconfig from '../config/auth';
+import AppError from '@shared/errors/AppError';
 
 interface TokenPayload {
   iat: number;
